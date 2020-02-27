ec9bb46af7c216eb1e8d139158d5dea74dbf60b9
Iago Breno <ibacaraujo@gmail.com>
2020-02-26 23:36:22 -0300

    Add the input as a uniform distribution

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import torch
 import numpy as np
 
 def get_distribution_sampler(mu, sigma):
-  return lambda x: torch.Tensor(np.random.normal(mu, sigma, (1, n)))
+    return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))
+
+def get_distribution_input_sampler():
+    return lambda m, n: torch.rand(m, n) 
